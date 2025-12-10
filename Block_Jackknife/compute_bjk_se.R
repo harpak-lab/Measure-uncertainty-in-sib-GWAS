b9f4fe866ad9e3b10306fbbae6e4d9a0403c59a9
Xinyi Miao <141753158+xinyiMeow@users.noreply.github.com>
2025-12-10 16:01:42 -0600

    Add compute_bjk_se.R for jackknife standard error calculation
    This script computes the jackknife standard error for beta matrices across chromosomes and saves the results in specified output directories.

---
+++ Block_Jackknife/compute_bjk_se.R
@@ -0,0 +1,55 @@
+library(data.table)
+library(matrixStats)
+library(optparse)
+
+option_list <- list(
+  make_option(c("-t", "--trait"), type="character", help="Trait name"),
+  make_option(c("-n", "--n_families"), type="integer", help="Number of families"),
+  make_option(c("-i", "--input_dir"), type="character", help="Directory containing beta matrices"),
+  make_option(c("-o", "--out_dir"), type="character", help="Output directory")
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+m <- 500
+d <- 500
+r <- opt$n_families - d
+
+dir.create(opt$out_dir, showWarnings = FALSE)
+bjk_list <- list()
+
+for(chr in 1:22){
+  fname <- file.path(opt$input_dir, paste0("beta_matrix.chr", chr, ".txt"))
+  
+  if(!file.exists(fname)) {
+    warning(paste("Missing file:", fname))
+    next
+  }
+
+  beta_matrix <- fread(fname) # fread is faster than read.table
+  
+  # Convert to matrix (excluding SNP col)
+  beta_vals <- as.matrix(beta_matrix[, -1, with=FALSE])
+  
+  beta_means <- rowMeans(beta_vals, na.rm = TRUE)
+  ssq <- (beta_vals - beta_means)^2
+  ssq_sum <- rowSums(ssq, na.rm = TRUE)
+
+  # Jackknife Variance Formula
+  jack_var <- (r / (d * (m - 1))) * ssq_sum
+  jack_se  <- sqrt(jack_var)
+
+  bjk_df <- data.frame(SNP = beta_matrix$SNP, bjk_se = jack_se)
+  
+  # Save individual chromosome
+  write.table(bjk_df, file.path(opt$out_dir, paste0(opt$trait, "_chr", chr, ".bjk_se.txt")), 
+              row.names=FALSE, quote=FALSE)
+  
+  bjk_list[[chr]] <- bjk_df
+  message(paste("Finished CHR", chr))
+}
+
+# Bind and save all
+bjk_all <- do.call(rbind, bjk_list)
+write.csv(bjk_all, file.path(opt$out_dir, paste0(opt$trait, ".bjk_se.all_snps.csv")), 
+          row.names=FALSE, quote=FALSE)
